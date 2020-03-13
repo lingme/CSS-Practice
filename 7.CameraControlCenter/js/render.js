0d25180f56011af7e52672781300dd3d66eb25b2
lingme <kid--l@hotmail.com>
2020-03-13 23:56:40 +0800

    add vue component and windows onload method

---
+++ 7.CameraControlCenter/js/render.js
@@ -0,0 +1,23 @@
+Vue.component('button-counter', {
+    props: ['id', 'name'],
+    template: '<button>{{ name }}</button>'
+})
+
+const posts = [];
+
+window.onload = () => {
+    for (let index = 0; index < 60; index++) {
+        var item = {
+            id: index,
+            name: index.toString()
+        }
+        posts.push(item);
+    }
+}
+
+new Vue({
+    el: '#components-demo',
+    data: {
+        posts
+    }
+})
